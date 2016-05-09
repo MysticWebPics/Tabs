e875256110b72c8062694da6ff7b5ff1caf5a1a8
MysticWebPics <mystic.webpics@gmail.com>
2016-05-09 16:26:47 +0200

    init

---
+++ js/tabs.js
@@ -0,0 +1,9 @@
+$(function() {
+	$("#tabs ul li").click(function() {
+		var id = $(this).children().attr('href');
+		$("#tabs div.active").removeClass("active").toggle();
+		$("#tabs ul li.active").removeClass("active");
+		$(id).addClass("active").fadeToggle();
+		$(this).addClass("active");
+	});
+});
